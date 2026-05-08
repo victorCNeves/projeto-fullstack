24e9a325930e79038a96782c6a963e65fff73d7e
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-05-08 10:15:08 -0300

    feat: criação de componente de catálogo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
 import { useLoaderData } from 'react-router';
+import { Box, VStack } from '@chakra-ui/react';
+import Carrosel from '@/components/Carrossel';
 
 const Home = () => {
-  const data = useLoaderData();
-
-  return <>{data.nome}</>;
+  const { generos } = useLoaderData();
+  console.log(generos);
+  return (
+    <Box minH="100vh" w="full">
+      <VStack align="stretch">
+        {generos.map((genero, key) => (
+          <Carrosel
+            titulo={genero.name}
+            filmes={genero.movies.results}
+            key={key}
+          />
+        ))}
+      </VStack>
+    </Box>
+  );
 };
 
 export default Home;
