5489fe7752cd407dce9b6c859051aa3ab6661049
victorCNeves <105159565+victorCNeves@users.noreply.github.com>
2026-05-11 16:09:45 -0300

    feat: mostrando mensagem de que não possui favoritados caso lista esteja vazia

--- src/pages/Favoritados/Favoritados.jsx
+++ src/pages/Favoritados/Favoritados.jsx
@@ -1,19 +1,30 @@
 import CardFilme from '@/components/CardFilme';
 import { FavoritadosContext } from '@/contexts/FavoritadosContext';
-import { Box, Container, SimpleGrid } from '@chakra-ui/react';
+import { Box, Container, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
 import { useContext } from 'react';
 
 const Favoritados = () => {
   const { favoritados } = useContext(FavoritadosContext);
   return (
     <Box bg="black" minH="100vh" py={10}>
-      <Container maxW="container.xl">
-        <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4, xl: 5 }} gap={6}>
-          {favoritados.map((filme) => (
-            <CardFilme key={filme.id} filme={filme} />
-          ))}
-        </SimpleGrid>
-      </Container>
+      {favoritados && favoritados.length > 0 ? (
+        <Container maxW="container.xl">
+          <SimpleGrid columns={{ base: 1, sm: 2, md: 3, lg: 4, xl: 5 }} gap={6}>
+            {favoritados.map((filme) => (
+              <CardFilme key={filme.id} filme={filme} />
+            ))}
+          </SimpleGrid>
+        </Container>
+      ) : (
+        <Flex
+          minW="100%"
+          minH="80vh"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Heading>Você não possui filmes favoritados.</Heading>
+        </Flex>
+      )}
     </Box>
   );
 };
